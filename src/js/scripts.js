bea92f6bec396819aaa3a1c6c682f9c6470d174d
Bryce Kirk <info@brycekirk.com>
2020-12-01 09:59:50 -0700

    start of new navigation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -95,3 +95,24 @@ $(window).on('scroll', () => {
 
   hero.css('background-position-y', imgPos);
 });
+
+// Dropdown
+$(document).on('click', '.dropdown-menu', (e) => {
+  e.stopPropagation();
+});
+
+// Drawer
+('[data-trigger]').on('click', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  var offcanvas_id =  $(this).attr('data-trigger');
+  $(offcanvas_id).toggleClass('show');
+  $('body').toggleClass('offcanvas-active');
+  $('.screen-overlay').toggleClass('show');
+}); 
+
+$('.btn-close, .screen-overlay').click(function(e){
+  $('.screen-overlay').removeClass('show');
+  $('.mobile-offcanvas').removeClass('show');
+  $('body').removeClass('offcanvas-active');
+}); 
