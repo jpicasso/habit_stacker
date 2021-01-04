fc0f5754fdc38bcd91bba60e0dbaaab61127f476
Bryce Kirk <info@brycekirk.com>
2021-01-04 11:45:29 -0700

    Fix sidebar nav

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,15 +12,26 @@ window.addEventListener('scroll', event => {
   let fromTop = window.scrollY;
   accTitles.removeClass('active');
 
-  mainNavLinks.forEach(link => {
+  // console.log(fromTop);
+
+  mainNavLinks.forEach((link, i) => {
     let section = $(link.hash);
 
-    if (section !== null) {
-      if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+    if (section.length > 0) {
+
+      if (i == 2) {
+        console.log(`From top: ${fromTop}`);
+        console.log(`between ${section[0].offsetTop} and ${section[0].offsetTop + section[0].offsetHeight}`);
+      }
+
+      if (
+        section[0].offsetTop - 65 <= fromTop && 
+        section[0].offsetTop + section[0].offsetHeight >= fromTop
+      ) {
         link.classList.add('active');
   
         // Add and open parent accordion
-        let accParent = $(link).parents('.collapse')
+        let accParent = $(link).parents('.collapse');
         accParent.prev().addClass('active');
         accParent.collapse('show');
   
@@ -33,18 +44,18 @@ window.addEventListener('scroll', event => {
 
 // Smooth scroll
 $('.accordion ul li a').on('click', e => { 
-  e.preventDefault();
-  let sub = $(e.target).attr('href');
+  // e.preventDefault();
+  let sub = $(e.target).attr('href').split('.html')[1];
   
   $('html, body').animate({
-    scrollTop: $(sub).offset().top
+    scrollTop: $(sub).offset().top - 50
   }, 500);
 
   // Active class on accordion parent after opening event finishes (shown.bs.collapse)
-  let parentAcc = $(e.target).parents('.collapse');
-  parentAcc.on('shown.bs.collapse', () => {
-    parentAcc.prev().addClass('active')
-  })
+  // let parentAcc = $(e.target).parents('.collapse');
+  // parentAcc.on('shown.bs.collapse', () => {
+  //   parentAcc.prev().addClass('active')
+  // })
 });
 
 // Homepage hero parallax  
